bd266375a3d360f94a4ba0eb2c1b8603b8e507fd
Gabriel Guerra <gfranklings@gmail.com>
2021-11-21 11:42:17 -0400

    validator: between.rule: removed unnecesary console.log

--- src/validator/rules/between.rule.ts
+++ src/validator/rules/between.rule.ts
@@ -1,4 +1,3 @@
 export default (value: string, min: number, max: number): boolean => {
-  console.log(value.length < max && value.length > min, value.length, max, min);
   return value.length < max && value.length > min;
 };
