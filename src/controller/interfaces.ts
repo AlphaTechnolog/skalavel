222c949b1c6c408b3cd06fca5a07db178df91ee3
Gabriel Guerra <gfranklings@gmail.com>
2021-11-30 23:19:10 -0400

    created the method `created` for the controller as created lifecycle

--- src/controller/interfaces.ts
+++ src/controller/interfaces.ts
@@ -11,8 +11,32 @@ export type IControllerConstructor = new (req: Request) => IController;
 export interface IController {
   req: Request;
   _register: IControllerRegister;
+  /**
+   * A lifecycle to be executed at create the controller
+   */
+  created (): void;
+  /**
+   * Make a http response
+   * @param response The response string
+   * @param headers The headers object
+   * @param statuscode The statuscode of the response
+   */
   _rawRes(response: any, headers: any, statuscode: number): void;
+  /**
+   * Make a configured response to be as html text
+   * @param response The response string
+   * @param statuscode The statuscode of the response
+   */
   htmlRes(response: string, statuscode?: number): void;
+  /**
+   * Make a response in json format
+   * @param response The json object
+   * @param statuscode The statuscode of the response
+   */
   jsonRes(response: any, statuscode?: number): void;
+  /**
+   * Redirect to another uri
+   * @param url The url to redirect
+   */
   redirect(url: string): void;
 }
